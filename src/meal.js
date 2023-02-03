9fc77eefa51442312ca32d7633af3b154d4f34b6
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-02-03 15:29:33 -0500

    Updates to meal.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,54 @@
-function nameMenuItem() {
+function nameMenuItem(menuItemName) {
+  //var updatedMenuName = (`Delicious ${menuItemName}`);
+  return `Delicious ${menuItemName}`
+}
 
+function createMenuItem(nameOfMenuItem,priceOfMenuItem,typeOfMenuItem) {
+  var newMenuItem = { name: nameOfMenuItem,
+  price: priceOfMenuItem,
+  type: typeOfMenuItem
+}
+if (nameOfMenuItem||priceOfMenuItem||typeOfMenuItem){
+  return newMenuItem
+} else {
+  return {}
+}
+}         
+
+
+function addIngredients(ingredient,array){
+  if (!array.includes(ingredient)) {
+  array.push(ingredient);
+  return array;
+}
+}
+
+function formatPrice(initialPrice){
+  //var formattedPrice = (`$${initialPrice}`);
+  return (`$${initialPrice}`);
+}
+function decreasePrice(decreasedPrice){
+  var newDecreasedPrice = (decreasedPrice-decreasedPrice*.10)
+  return newDecreasedPrice
+
+}       
+function createRecipe(title,ingredients,menuItemType){
+  var createRecipeObject = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+  return createRecipeObject
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
