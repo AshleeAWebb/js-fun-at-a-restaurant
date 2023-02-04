fa66c87e81eb54596ba9925c48906c628952f0d2
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-02-04 14:30:19 -0500

    Update all functions.

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,33 @@
+function createRestaurant(restaurantName) {
+  return {name: restaurantName, 
+    menus: 
+    {breakfast:[], 
+      lunch: [], 
+      dinner: [] 
+    }
+  
+  };
+}
+
+function addMenuItem(restaurant, newItem) {
+  if (!restaurant.menus[newItem.type].includes(newItem))
+return restaurant.menus[newItem.type].push(newItem)
+}
+
+function removeMenuItem(restaurant, itemRemove, menuItemType) {
+  for (var i = 0; i < restaurant.menus[menuItemType].length; i++) {
+    if (restaurant.menus[menuItemType][i].name === itemRemove) {
+      restaurant.menus[menuItemType].splice(i, 1);
+      return `No one is eating our ${itemRemove} - it has been removed from the ${menuItemType} menu!`;
+    }
+  }
+    return `Sorry, we don't sell ${itemRemove}, try adding a new recipe!`
+  }
+  
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant, 
+  addMenuItem,
+  removeMenuItem
 }
