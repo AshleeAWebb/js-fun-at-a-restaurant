b1404e4145705b22f1379dfd8ff2f6494d3fc99f
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-02-04 12:35:48 -0500

    Update all functions.

--- src/order.js
+++ src/order.js
@@ -1,7 +1,41 @@
+function takeOrder(order,takeOutOrder){
+  if (takeOutOrder.length <= 2) {
+  return takeOutOrder.push(order)
+}
+}
+
+function refundOrder(orderNumber, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber === orderNumber) {
+      return deliveryOrders.splice(i, 1);
+    }
+  }
+}
+
+function listItems(deliveryOrders) {
+  var deliveryOrderItems = '';
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    deliveryOrderItems += deliveryOrders[i].item;
+    if (i < deliveryOrders.length - 1) {
+      deliveryOrderItems += ", ";
+    }
+  } 
+  return deliveryOrderItems;
+}
+function searchOrder (deliveryOrders, item) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].item === item) {
+      return true;
+    } 
+  }
+  return false;
+}
+
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
