f5ff06f7112e04838ce032e3ad369b5d530bc0ab
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-02-05 18:59:19 -0500

    Incomplete Chef.js functions

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,34 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
+   }
+  
+  greetCustomer(customerName, morning = false){
+   if (morning) {
+    return `Good morning, ${customerName}!`;
+   } else {
+    return `Hello, ${customerName}!`;
+  }
+   }
+  
+
+  
+  addFoodItem(foodItem)  {
+    this.restaurant.menu = [];
+   this.restaurant.menu.push(foodItem);
+  }
+  addMenuItem(foodItem) {
+    this.restaurant.menu.push(foodItem);
   }
 }
-
-module.exports = Chef;
+  module.exports = Chef;
+//   checkForFood(itemOnMenu) {
+//     for (let i = 0; i < this.restaurant.menu.length; i++) {
+//       if (this.restaurant.menu[i].name === itemOnMenu.name) {
+//         return `Yes, we're serving ${itemOnMenu.name} today!`;
+//       }
+//     }
+//     return `Sorry,we aren't serving ${itemOnMenu.name} today.`;
+//   }
+// }
